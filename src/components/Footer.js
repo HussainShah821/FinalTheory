0593c8c9939e203ddac732eb9d71b42667219120
Hussainshah821 <ssyedhussain821@gmail.com>
2023-12-20 16:50:53 +0500

    Final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from "./Assets/Images/logo.png";
+import {unstable_renderSubtreeIntoContainer} from "react-dom";
 
 function Footer() {
     return (
-        <footer>
-            <p style={{backgroundColor: "#757272", padding: "3rem"}}></p>
-        </footer>
+
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"  >
+            <Container>
+                <Navbar.Brand href="#home" className="d-flex align-items-center">
+                    <img src={Logo} alt="Logo" className="mr-2" />
+                    Web3Makr
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link >White paper</Nav.Link>
+                        <Nav.Link >Services</Nav.Link>
+                        <Nav.Link >Tool</Nav.Link>
+                        <Nav.Link >Contact us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
