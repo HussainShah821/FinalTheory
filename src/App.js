c8d62337b7f1bde0e8ac6d60b8af96957eb21b20
Hussainshah821 <ssyedhussain821@gmail.com>
2023-12-20 15:42:48 +0500

    Hero Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import MyNavbar from './components/Navbar';
 import Footer from './components/Footer';
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
    
       <div>
         <MyNavbar />
-
+        <HeroSection />
 
 
         <Footer />
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Welcome from '../components/Welcome';
-import Button from 'react-bootstrap/Button';
-import Image from '../components/Assets/Images/Group 10.png';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import image from './Assets/Images/Play 02.png';
+import web from './Assets/Images/Web.png';
 
-export const HeroSection = () => {
-  return (
-    <>
-      <div className="container row pt-5 ml-3" style={{ marginLeft: '3%' }}>
-        <div className="col-md-8">
-          <h1 className='font-weight-bold'>The Fast Pinpoint</h1>
-          <h1 className="text-danger font-weight-bold">Get Started -&gt;</h1>
-          <h1 className="font-weight-bold">Tracking & Reservation.</h1>
-        </div>
-        <div className="col-md-4">
-          <Link to="/about">
-            <img
-              src={Image}
-              alt="Your Alt Text Here"
-              className="img-fluid"
-              style={{ maxWidth: '90%', height: 'auto', marginLeft: '40%' }}
-            />
-          </Link>
-        </div>
-      </div>
+function HeroSection() {
+    return (
+        <Container fluid style={{ backgroundColor: "#344767", padding: "2%" }}>
+            <Row>
+                <Col className="text-center">
+                    <h1 className="text-white">
+                        Web3Makr: Redefining <br />
+                        How You Create
+                    </h1>
+                    <p className="text-white" style={{ padding: "2%" }}>
+                        Unlock the blockchain potential to create <br />
+                        Web3 Applications
+                    </p>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <Image src={image} alt="Play" fluid />
+                    </div>
+                    <div className="d-flex align-items-center justify-content-center" style={{paddingTop: "6rem", paddingBottom:"4rem"}}>
+                        <Image src={web} alt="Web" fluid />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 
-      <div style={{ marginRight: '7rem', marginLeft: '7rem', marginTop: '2rem', marginBottom: '2rem' }}>
-        <h4>Pinpoint is the fastest tracking and reservation software solution for personal and enterprise vehicles for institutes, offices, and companies with live tracking, history recording, estimated arrival time, and usage report generation and much more features</h4>
-      </div>
-    </>
-  );
-};
-
-      
+export default HeroSection;
